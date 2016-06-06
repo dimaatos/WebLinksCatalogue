b08b41e5c5ca2893b706e89430b394bd61569c89
gogolevsergey <gogolev.sergey@gmail.com>
2016-06-06 18:18:00 +0300

    Model and list in view

--- WebLinksCatalogue.WebUI/Controllers/HomeController.cs
+++ WebLinksCatalogue.WebUI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebLinksCatalogue.WebUI.Models;
 
 namespace WebLinksCatalogue.WebUI.Controllers
 {
@@ -11,7 +12,7 @@ namespace WebLinksCatalogue.WebUI.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(new List<WebLinkModel> { new WebLinkModel() { Name="1" } });
         }
     }
 }
---
+++ WebLinksCatalogue.WebUI/Models/WebLinkModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLinksCatalogue.WebUI.Models
+{
+    public class WebLinkModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public int Views { get; set; }
+        public string ThumbnailURL { get; set; }
+    }
+}

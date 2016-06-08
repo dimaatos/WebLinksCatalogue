fddd505e558da6af24fd1d73dc7f8c229455b14e
gogolevsergey <gogolev.sergey@gmail.com>
2016-06-08 21:43:59 +0300

    Create, menu

--- WebLinksCatalogue.WebUI/Controllers/HomeController.cs
+++ WebLinksCatalogue.WebUI/Controllers/HomeController.cs
@@ -11,13 +11,39 @@ namespace WebLinksCatalogue.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        
+
         // GET: Home
         public ActionResult Index()
         {
+            return RedirectToAction(nameof(Links));
+        }
+
+        public ActionResult Links()
+        {
             EfDbContext db = new EfDbContext();
-            
+
             return View(db.Links/*new List<Link> { new Link() { Title="1" } }*/);
+        }
+
+        public ActionResult Categories()
+        {
+            EfDbContext db = new EfDbContext();
+
+            return View(db.Categories/*new List<Link> { new Link() { Title="1" } }*/);
+        }
+
+        public ActionResult CreateLink()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateLink(Link newLink)
+        {
+            EfDbContext db = new EfDbContext();
+            db.Links.Add(newLink);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
